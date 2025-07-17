fe26da0c55caa806534615fd868a6a315032827b
Romanrose <romanrose678@gmail.com>
2025-07-17 21:14:46 +0800

    list

---
+++ 1.py
@@ -0,0 +1,34 @@
+#列表
+[]
+
+classmates = ['Michael', 1, 'Curry']
+print(classmates)
+
+# print(len(classmates)) #3
+
+
+
+# for i in range(0, 3):
+#     print(classmates[i])
+
+# for i in range(-3, 0):
+#     print(classmates[i])
+
+# 追加元素到末尾
+# classmates.append("Adam")
+
+# print(classmates)
+
+# classmates.insert(0, 'jack')
+
+# print(classmates)
+
+# classmates.pop()
+
+# print(classmates.pop())
+
+print(classmates.pop(1))
+
+p = ['w', 'q', 'e']
+q = ['z', 'x', 'c', p]
+print(q)
